56ce4f417ad1e2f1dccf50e1b031c2f9f1b8879d
ySeungJE <dbstmdwp98@naver.com>
2022-02-09 07:31:48 +0900

    just

--- String__10.java
+++ String__10.java
@@ -1,15 +1,12 @@
 package BeakJoon_Algorithm;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class String__10 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		char[] word;
 		char test;
 		boolean[] b_arr;
