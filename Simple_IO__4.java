3c3702ae3f6e50249843f86a4941f67f21bfb133
ySeungJE <dbstmdwp98@naver.com>
2022-02-10 09:27:23 +0900

    2/10-09:27 upload

---
+++ Simple_IO__4.java
@@ -0,0 +1,11 @@
+package BeakJoon_Algorithm;
+
+public class Simple_IO__4 {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
---
+++ Simple_IO__5.java
@@ -0,0 +1,14 @@
+package BeakJoon_Algorithm;
+
+import java.util.Scanner;
+
+public class Simple_IO__5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a+b);
+	}
+}
